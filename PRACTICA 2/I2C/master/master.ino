db6ad5117f80a6356041ace44b08c4eee75c0e7b
Sheila-Amaya <2851326790117@ingenieria.usac.edu.gt>
2024-02-19 17:19:25 -0600

    ...
    pendiente

---
+++ PRACTICA 2/I2C/master/master.ino
@@ -0,0 +1,15 @@
+//pantalla LCD
+//manejo botones del menu
+//historial de cajas
+//control de banda transportadora
+//sensor ultrasonico
+#include <Wire.h>
+
+void setup() {
+  
+
+}
+
+void loop() {
+  
+}
---
+++ PRACTICA 2/I2C/slave/slave.ino
@@ -0,0 +1,16 @@
+
+//sensor de color
+//sensor de tamanio
+//mecanismo para separar cajas (servos)
+#include <Wire.h>
+
+
+void setup() {
+  
+
+}
+
+void loop() {
+  
+
+}
