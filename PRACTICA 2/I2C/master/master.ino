506b3de227edad83999691d47392e9b64fdd1ee8
brandonT2002 <brandonteja02@gmail.com>
2024-02-23 09:21:19 -0600

    [update] I2C

--- PRACTICA 2/I2C/master/master.ino
+++ PRACTICA 2/I2C/master/master.ino
@@ -1,15 +1,17 @@
-//pantalla LCD
-//manejo botones del menu
-//historial de cajas
-//control de banda transportadora
-//sensor ultrasonico
 #include <Wire.h>
 
-void setup() {
-  
+#define SLAVE_ADDRESS 9 // Dirección del esclavo
 
+int numToSend = 2;  // Número a enviar al esclavo (puede ser cualquier número)
+
+void setup() {
+  Wire.begin(); // Inicializar el bus I2C
+  Serial.begin(9600); // Inicializar la comunicación serial
 }
 
 void loop() {
-  
+  Wire.beginTransmission(SLAVE_ADDRESS); // Iniciar transmisión con el esclavo
+  Wire.write(numToSend); // Enviar el número al esclavo
+  Wire.endTransmission(); // Finalizar transmisión
+  delay(1000); // Esperar un segundo antes de enviar el próximo número
 }
--- PRACTICA 2/I2C/slave/slave.ino
+++ PRACTICA 2/I2C/slave/slave.ino
@@ -1,16 +1,48 @@
-
-//sensor de color
-//sensor de tamanio
-//mecanismo para separar cajas (servos)
 #include <Wire.h>
 
+#define SLAVE_ADDRESS 9 // Dirección del esclavo
+
+void function1(); // Declaración de función
+void paradaEmergencia(); // Declaración de función
 
 void setup() {
-  
-
+  Wire.begin(SLAVE_ADDRESS); // Inicializar el esclavo con la dirección especificada
+  Wire.onReceive(receiveEvent); // Configurar la función para manejar los datos recibidos
+  Serial.begin(9600); // Inicializar la comunicación serial
 }
 
 void loop() {
-  
+  // El esclavo no necesita ejecutar nada en el loop ya que responderá a las solicitudes del maestro en receiveEvent
+}
 
+void receiveEvent(int numBytes) {
+  while(Wire.available() > 0) { // Mientras haya datos disponibles para leer
+    int numReceived = Wire.read(); // Leer el número enviado por el maestro
+    // Ejecutar una función específica basada en el número recibido
+    switch(numReceived) {
+      case 1:
+        function1();
+        break;
+      case 2:
+        paradaEmergencia();
+        break;
+      case 3:
+        function1();
+        break;
+      // Puedes agregar más casos para ejecutar otras funciones según el número recibido
+      default:
+        // Manejar caso no reconocido
+        break;
+    }
+  }
+}
+
+void function1() {
+  Serial.println("Función 1 ejecutada.");
+  // Código de la función 1
+}
+
+void paradaEmergencia(){
+  Serial.println("se realizo una perada de merfgencia.");
+  delay(1000);  
 }
